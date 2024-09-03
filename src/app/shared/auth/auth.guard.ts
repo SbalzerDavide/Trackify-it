1a3ae7c8cafa4abddd32bca495dc26df6e35c923
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-03 16:58:51 +0200

    fix for reirect to login without auth

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -1,12 +1,13 @@
 import { inject } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from "@angular/router";
-import { SupabaseService } from "../../supabase.service";
+import { ActivatedRouteSnapshot, CanActivateFn, RedirectCommand, Router, RouterStateSnapshot } from "@angular/router";
+import { SupabaseService } from "../supabase/supabase.service";
 
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) =>{
   const supabaseService = inject(SupabaseService)
+  const router = inject(Router)
   if(supabaseService.session){
     return true
   } else{
-    return false
+    return new RedirectCommand(router.parseUrl('login'))
   }
 }
