00c1290b7b97cfd15e5454fd85d5c2cc143b2b88
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-19 12:09:37 +0200

    add home and personal area

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Profile, SupabaseService } from '../../supabase.service';
+import { AuthService } from '../../shared/auth/auth.service';
+
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  supabaseService = inject(SupabaseService)
+  authService = inject(AuthService)
 
 }
