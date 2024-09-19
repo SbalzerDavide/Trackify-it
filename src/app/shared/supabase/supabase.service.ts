9716fcf2b8309e2c4df0ec1159cb30afeeb26711
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-19 12:09:54 +0200

    move file

---
+++ src/app/shared/supabase/supabase.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, signal } from '@angular/core'
+import {
+  AuthSession,
+  createClient,
+  SupabaseClient,
+} from '@supabase/supabase-js'
+import { environment } from '../../../environments/environment'
+
+export interface Profile {
+  id?: string
+  username: string
+  website: string
+  avatar_url: string
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  supabase: SupabaseClient
+  session: AuthSession | null = null
+  private profile = signal<Profile | null>(null)
+  activeProfile = this.profile.asReadonly()
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  }
+
+
+  // api
+  async getFromTable(table: string, columns: string){
+    // const data = await this.supabase.from('basic_activity_exercise').select(`name, description, cal`)
+    //   .eq('user_id', this.session?.user.id)
+    // console.log(data);
+    
+    return this.supabase.from(table).select(columns)
+  }
+
+}
