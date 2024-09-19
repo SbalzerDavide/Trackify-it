9821461fa305a8e41627037ede2d59e060949925
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-19 12:09:06 +0200

    add card

---
+++ src/app/shared/lib/card/card.component.ts
@@ -0,0 +1,21 @@
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+  title = input.required<string>();
+  description = input.required<string>();
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+}
