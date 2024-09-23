1b0927cb0fd9d87ab3c1e50a7ffec86a46947336
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-23 17:17:31 +0200

    manage change quantity

--- src/app/shared/lib/card/card.component.ts
+++ src/app/shared/lib/card/card.component.ts
@@ -1,21 +1,29 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, MatIconModule, MatButtonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
   title = input.required<string>();
-  description = input.required<string>();
+  description = input<string>();
+  quantity = input<number>()
   longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
   Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
   usually kept as a companion animal or for showing.`;
+
+  updateQuantity = output<number>()
+
+
 }
