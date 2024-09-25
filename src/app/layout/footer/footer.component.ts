8fce646e4394f6c993d669b143e1b0f4a0d47831
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-25 16:17:33 +0200

    add new activity from footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { FormActivityComponent } from '../../components/activity/form-activity/form-activity.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  readonly dialog = inject(MatDialog);
 
+  openDialog() {
+    this.dialog.open(FormActivityComponent);
+  }
 }
