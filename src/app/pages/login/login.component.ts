4b1d17b8e6d1ff9438e00824258a066aad291968
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-09-30 17:22:39 +0200

    clean code

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,8 +8,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @Component({
