5ee4be168df408736cd34965a7342664ea63fd07
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-10-15 16:11:20 +0200

    use icon

--- src/app/shared/lib/custom-card/custom-card.component.ts
+++ src/app/shared/lib/custom-card/custom-card.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-custom-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule],
   styleUrl: './custom-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,  templateUrl: './custom-card.component.html',
 })
