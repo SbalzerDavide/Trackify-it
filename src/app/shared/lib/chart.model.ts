53aabcaba2a21242bf5a8444eb38ae7220acaf10
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-10-15 12:39:09 +0200

    improve interface

--- src/app/shared/lib/chart.model.ts
+++ src/app/shared/lib/chart.model.ts
@@ -3,7 +3,17 @@ export interface ChartInfo {
   exercise_id: string | undefined,
   is_range_absolute?: Boolean,
   range_type: 'daily' | 'weekly' | 'monthly' | 'annual',
-  show_in_dashboard?: boolean
+  show_in_dashboard?: boolean,
+  startRange?: Date,
+  endRange?: Date,
+  data?: {
+    type?: 'line' | 'bar', 
+    xType?: 'value' | 'category' | 'time' | 'log',
+    yType?: 'value' | 'category' | 'time' | 'log',
+    xdata: string[],
+    data?: any,
+    goal: number | undefined
+  } 
 }
 
 export const CHART = 'chart'
