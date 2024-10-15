0c72d78ceecc3dfddd628a65d56047ded3920d7a
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-10-15 16:08:16 +0200

    add swiper component

---
+++ src/app/shared/lib/swiper/swiper.component.ts
@@ -0,0 +1,13 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper',
+  standalone: true,
+  imports: [],
+  templateUrl: './swiper.component.html',
+  styleUrl: './swiper.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SwiperComponent {
+
+}
