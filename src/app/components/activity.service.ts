fe0a1a0eda860c92840ba782ff8775ce70fb46e2
SbalzerDavide <sbalzerdavide@hotmail.it>
2024-12-14 07:10:01 +0100

    fix order by id on fetch and use prettier

--- src/app/components/activity.service.ts
+++ src/app/components/activity.service.ts
@@ -1,91 +1,95 @@
 import { inject, Injectable, signal } from '@angular/core';
 
 import { SupabaseService } from '../shared/supabase/supabase.service';
-import { ACTIVITIES } from './activity.model'; 
+import { ACTIVITIES } from './activity.model';
 import { EXERCISES } from './exercises.model';
 import { BASIC_ACTIVITY_EXERCISE } from './basic-activity.model';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ActivityService {
+  private userActivities = signal<any[]>([]);
+  private allActivities = signal<any[]>([]);
+  loadedActivities = this.userActivities.asReadonly();
+  loadedAllActivities = this.allActivities.asReadonly();
 
-  private userActivities = signal<any[]>([])
-  private allActivities = signal<any[]>([])
-  loadedActivities = this.userActivities.asReadonly()
-  loadedAllActivities = this.allActivities.asReadonly()
-  
-  updateActivities : Subject<any> = new Subject
+  updateActivities: Subject<any> = new Subject();
 
-  startRange = signal<Date>(new Date())
-  endRange = signal<Date>(new Date())
-    
-  supabaseService = inject(SupabaseService)
-    
+  startRange = signal<Date>(new Date());
+  endRange = signal<Date>(new Date());
+
+  supabaseService = inject(SupabaseService);
+
   constructor() {}
 
   // SELECT
-  async fetchFilteredActivities(exerciseId: string, startDate: Date, endDate: Date){
+  async fetchFilteredActivities(
+    exerciseId: string,
+    startDate: Date,
+    endDate: Date
+  ) {
     const { data } = await this.supabaseService.supabase
       .from(ACTIVITIES)
-      .select(`date, id, quantity, exercise_id,
-        ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`)
+      .select(
+        `date, id, quantity, exercise_id,
+        ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`
+      )
       .gte('date', this.pgFormatDate(startDate))
       .lte('date', this.pgFormatDate(endDate))
       .eq('exercise_id', exerciseId)
-      .order('date', { ascending: true })
-    if(data){
-      return data
-    } else{
-      return []
+      .order('date', { ascending: true });
+    if (data) {
+      return data;
+    } else {
+      return [];
     }
   }
 
   // SELECT BETWEEN DATE
-  async fetchRangeActivities(startDate: Date, endDate: Date){
+  async fetchRangeActivities(startDate: Date, endDate: Date) {
     const { data } = await this.supabaseService.supabase
       .from(ACTIVITIES)
-      .select(`date, id, quantity, exercise_id,
-        ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`)
-        .gte('date', this.pgFormatDate(startDate))
-        .lte('date', this.pgFormatDate(endDate))
+      .select(
+        `date, id, quantity, exercise_id,
+        ${EXERCISES}( number_of_repetitions, name, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`
+      )
+      .gte('date', this.pgFormatDate(startDate))
+      .lte('date', this.pgFormatDate(endDate))
       .order('date', { ascending: true })
+      .order('id', { ascending: true });
 
-          
-    if(data){
-      return data
-    } else{
-      return []
+    if (data) {
+      return data;
+    } else {
+      return [];
     }
   }
 
   // INSERT
-  async addActivity(activity: any){
-    const activityDate = this.pgFormatDate(activity.date) 
-    activity.date = activityDate           
-    
+  async addActivity(activity: any) {
+    const activityDate = this.pgFormatDate(activity.date);
+    activity.date = activityDate;
+
     const { error } = await this.supabaseService.supabase
       .from(ACTIVITIES)
-      .insert(activity)
-      .select(`date, id, quantity, exercise_id,
-      ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`)
+      .insert(activity).select(`date, id, quantity, exercise_id,
+      ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`);
     if (error) {
       console.error(error.message);
     }
-
   }
 
   // UPDATE
-  async updateActivity(newVal: {}, id: string){
+  async updateActivity(newVal: {}, id: string) {
     const { data, error } = await this.supabaseService.supabase
       .from(ACTIVITIES)
       .update(newVal)
-      .eq('id', id)
-      .select(`date, id, quantity, exercise_id,
-      ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`)
+      .eq('id', id).select(`date, id, quantity, exercise_id,
+      ${EXERCISES}( number_of_repetitions, ${BASIC_ACTIVITY_EXERCISE}( name, cal))`);
 
-    if(data){
+    if (data) {
       // update local data
       // data.forEach((el)=>{
       //   const updated = el;
@@ -97,20 +101,20 @@ export class ActivityService {
       //   }
       // })
     }
-    
+
     if (error) {
       console.error(error.message);
-    }  
+    }
   }
 
   // DELETE
-  async deleteActivity(id: string){
-    try{
+  async deleteActivity(id: string) {
+    try {
       await this.supabaseService.supabase
         .from(ACTIVITIES)
         .delete()
-        .eq('id', id)     
-      
+        .eq('id', id);
+
       // update local data
       // const deletedIndex = this.loadedActivities().findIndex(el => el.id === id)
       // if(deletedIndex >= 0){
@@ -118,35 +122,40 @@ export class ActivityService {
       //   updatedUserActivities.splice(deletedIndex, 1)
       //   this.userActivities.set(updatedUserActivities)
       // }
-    } catch(error){
-      console.error(error)
+    } catch (error) {
+      console.error(error);
     }
   }
 
-  getGroupingActivity(data: any[]){
-    let groupingList: any[] = []
-    data.forEach(el => {
-      const check = groupingList.findIndex((element) => element.exercise_id === el.exercise_id)
-      
-      if(check >=0){
-        groupingList[check].quantity = groupingList[check].quantity + el.quantity
-      } else{
-        groupingList.push({...el})
+  getGroupingActivity(data: any[]) {
+    let groupingList: any[] = [];
+    data.forEach((el) => {
+      const check = groupingList.findIndex(
+        (element) => element.exercise_id === el.exercise_id
+      );
+
+      if (check >= 0) {
+        groupingList[check].quantity =
+          groupingList[check].quantity + el.quantity;
+      } else {
+        groupingList.push({ ...el });
       }
-    })
+    });
     return groupingList;
   }
 
-  private pgFormatDate(date:Date) {
+  private pgFormatDate(date: Date) {
     /* Via http://stackoverflow.com/questions/3605214/javascript-add-leading-zeroes-to-date */
-    function zeroPad(d:number) {
-      return ("0" + d).slice(-2)
+    function zeroPad(d: number) {
+      return ('0' + d).slice(-2);
     }
-  
-    var parsed = new Date(date)
-  
-    return [parsed.getUTCFullYear(), zeroPad(parsed.getMonth() + 1), zeroPad(parsed.getDate())].join("-");
-  }
 
-  
+    var parsed = new Date(date);
+
+    return [
+      parsed.getUTCFullYear(),
+      zeroPad(parsed.getMonth() + 1),
+      zeroPad(parsed.getDate()),
+    ].join('-');
+  }
 }
