5e5fa85b85b492bba7b9e5f5ce2a7ebc6321217a
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-01-04 10:34:25 +0100

    fix ux for insert activities

--- src/app/components/entities/form-entities/form-entities.component.ts
+++ src/app/components/entities/form-entities/form-entities.component.ts
@@ -1,13 +1,20 @@
 import { Component, computed, inject, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog'
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-
 
 import { EntitiesService } from '../../entities.service';
 import { BasicEntitiesService } from '../../basic-entities.service';
@@ -16,63 +23,74 @@ import { FormBasicActivityComponent } from '../../basic-activity-entity/form-bas
 @Component({
   selector: 'app-form-entities',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatDialogModule, MatInputModule, MatSelectModule, MatButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: './form-entities.component.html',
-  styleUrl: './form-entities.component.css'
+  styleUrl: './form-entities.component.css',
 })
-export class FormEntitiesComponent implements OnInit{
-  entitiesService = inject(EntitiesService)
-  basicEntitiesService = inject(BasicEntitiesService)
+export class FormEntitiesComponent implements OnInit {
+  entitiesService = inject(EntitiesService);
+  basicEntitiesService = inject(BasicEntitiesService);
 
-  constructor(public dialogRef: MatDialogRef<FormBasicActivityComponent>) { }
+  constructor(public dialogRef: MatDialogRef<FormBasicActivityComponent>) {}
 
   async ngOnInit() {
-    await this.basicEntitiesService.fetchBasicActivities()
+    await this.basicEntitiesService.fetchBasicActivities();
   }
 
   closeDialog() {
     this.dialogRef.close();
   }
 
-  basicActivities = computed(()=>{    
-    return this.basicEntitiesService.loadedBasicActivities().map((el)=>{
+  basicActivities = computed(() => {
+    return this.basicEntitiesService.loadedBasicActivities().map((el) => {
       return {
         value: el.id,
-        label: el.name
-      }
-    })
-  })
+        label: el.name,
+      };
+    });
+  });
 
-  insertEntityForm = new FormGroup({
-    basicEntity: new FormControl(''),
-    name: new FormControl(''),
-    unitValue: new FormControl('', {
-      validators: [Validators.required, Validators.min(1)]
-    }),
-  }, { validators: this.oneFieldOnlyValidator('basicEntity', 'name') })
+  insertEntityForm = new FormGroup(
+    {
+      basicEntity: new FormControl(''),
+      name: new FormControl(''),
+      unitValue: new FormControl('', {
+        validators: [Validators.required, Validators.min(1)],
+      }),
+    },
+    { validators: this.oneFieldOnlyValidator('basicEntity', 'name') }
+  );
 
-  async insertEntity(){
-    if(this.insertEntityForm.valid){
-      try{      
-        if(this.insertEntityForm.valid){
+  async insertEntity() {
+    if (this.insertEntityForm.valid) {
+      try {
+        if (this.insertEntityForm.valid) {
           const newEntity: {
             unit_value: string;
             name?: string;
             basic_entity_id?: string;
           } = {
             unit_value: this.insertEntityForm.value.unitValue!,
+          };
+          if (this.insertEntityForm.value.basicEntity) {
+            newEntity.basic_entity_id = this.insertEntityForm.value.basicEntity;
+          } else if (this.insertEntityForm.value.name) {
+            newEntity.name = this.insertEntityForm.value.name;
           }
-          if(this.insertEntityForm.value.basicEntity){
-            newEntity.basic_entity_id = this.insertEntityForm.value.basicEntity
-          } else if(this.insertEntityForm.value.name){
-            newEntity.name = this.insertEntityForm.value.name
-          }
-          await this.entitiesService.addEntity(newEntity)
-          this.entitiesService.fetchEntities()
-          this.closeDialog()        
+          await this.entitiesService.addEntity(newEntity);
+          this.entitiesService.fetchEntities();
+          this.closeDialog();
         }
-      } catch(error){
-        console.error(error)
+      } catch (error) {
+        console.error(error);
       }
     }
   }
@@ -81,13 +99,12 @@ export class FormEntitiesComponent implements OnInit{
     return (group: AbstractControl): ValidationErrors | null => {
       const field1Value = group.get(field1)?.value;
       const field2Value = group.get(field2)?.value;
-  
+
       if ((field1Value && field2Value) || (!field1Value && !field2Value)) {
         return { oneFieldOnly: true }; // Errore se entrambi o nessuno dei due sono popolati
       }
-  
+
       return null; // Valido
     };
   }
-
 }
